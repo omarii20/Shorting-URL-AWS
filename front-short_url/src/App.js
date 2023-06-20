2681bcf3feba0f59f71eb9a8b3f1bc4dd8858600
Mohamed Omarii <mohamed.44301hotmail.com>
2023-06-20 14:34:07 +0300

    added another div for long url

--- front-short_url/src/App.js
+++ front-short_url/src/App.js
@@ -3,20 +3,56 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  const [longUrl, setLongUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
 
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
+  const handleLongUrlChange = (event) => {
+    setLongUrl(event.target.value);
   };
 
-  const handleSubmit = async () => {
+  const handleShortUrlChange = (event) => {
+    setShortUrl(event.target.value);
+  };
+
+  const handleShortenUrl = async () => {
+    const API_URL = 'https://38rhg3gt1c.execute-api.us-east-1.amazonaws.com/dev/value';
+
     try {
-      const response = await axios.post(
-        'https://38rhg3gt1c.execute-api.us-east-1.amazonaws.com/dev',
-        { url: inputValue }
-      );
-      const urlFromResponse = response.data.url;
-      window.open(urlFromResponse, '_blank');
+      const response = await axios.post(API_URL, {
+        type: 'POST',
+        url_long: longUrl
+      });
+
+      const data = response.data;
+      const longerUrl = data.url_long;
+
+      if (!/^https?:\/\//i.test(longerUrl)) {
+        setLongUrl(longerUrl);
+      }
+
+      setShortUrl(data.url_short);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleGetOriginalUrl = async () => {
+    const API_URL = 'https://38rhg3gt1c.execute-api.us-east-1.amazonaws.com/dev/value';
+
+    try {
+      const response = await axios.post(API_URL, {
+        type: 'GET',
+        url_short: shortUrl
+      });
+
+      const data = response.data;
+      const longerUrl = data.url_long;
+
+      if (!/^https?:\/\//i.test(longerUrl)) {
+        setLongUrl(longerUrl);
+      }
+
+      setLongUrl(data.url_long);
     } catch (error) {
       console.error(error);
     }
@@ -24,18 +60,57 @@ function App() {
 
   return (
     <div className="app">
-      <div className='title'>
-          <h2>URL shortener</h2>
-          <h5>System for shortening URL, built in AWS cloud use Serverless Framework</h5>
-      </div> 
-      <div className='shortener'>
-        <form>
-          <input type="text" value={inputValue} onChange={handleChange} />
-          <button onClick={handleSubmit}>Redirect</button>
-        </form>
+      <div className="title">
+        <h1>URL Shortener</h1>
+        <p>System for shortening URL, built in AWS cloud use Serverless Framework.</p>
       </div>
-    </div>
 
+      <div className="shortener">
+        <div>
+          <input
+            type="text"
+            placeholder="Enter long URL"
+            value={longUrl}
+            onChange={handleLongUrlChange}
+          />
+          <button type="button" onClick={handleShortenUrl}>
+            Shorten
+          </button>
+        </div>
+      </div>
+
+      {shortUrl && (
+        <div className="result">
+          Short url: 
+          <a href={shortUrl} target="_blank" rel="noopener noreferrer">
+            {shortUrl}
+          </a>
+        </div>
+      )}
+
+      <div className="shortener">
+        <div>
+          <input
+            type="text"
+            placeholder="Enter short URL"
+            value={shortUrl}
+            onChange={handleShortUrlChange}
+          />
+          <button type="button" onClick={handleGetOriginalUrl}>
+            Retrieve
+          </button>
+        </div>
+      </div>
+
+      {longUrl && (
+        <div className="result">
+          Long url:
+          <a href={longUrl} target="_blank" rel="noopener noreferrer">
+            {longUrl}
+          </a>
+        </div>
+      )}
+    </div>
   );
 }
 
